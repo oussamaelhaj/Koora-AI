ce58ade892beeb8700cb07fd010c535ea1c21d40
oussamaelhaj <kifachtv24@gmail.com>
2025-10-13 20:05:40 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 import express from "express";
 import axios from "axios";
 import cors from "cors";
-import cheerio from "cheerio";
+import * as cheerio from "cheerio";
 import dotenv from "dotenv";
 
 // Charger les variables d'environnement
